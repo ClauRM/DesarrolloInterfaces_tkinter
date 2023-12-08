bceb3a3c072d5148e40c48683186809129f9d15e
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-12-08 20:38:43 +0100

    Se incluyen sobre los botones y bindings al hacer focusOut en los campos

--- 001-Interfaz/003-Interfaz con style y binds.py
+++ 001-Interfaz/003-Interfaz con style y binds.py
@@ -1,22 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
 
-def validaciones(evento):
-    pass
+def validarCampo(evento):
+    if len(campoNombre.get()) <= 0 or len(campoApellidos.get()) <= 0 or len(campoTelefono.get()) <= 0 or len(campoEmail.get()) <= 0:
+        mensajes.config(text="Todos los campos deben tener contenido")
     
 def cambiaEstilo():
-    print("Cambia el estilo")
-    estilo.theme_use(desplegable.get())
+    try:
+        estilo.theme_use(desplegable.get())
+        mensajes.config(text="Has cambiado el estilo")
+    except:
+        mensajes.config(text="Debes deleccionar un estilo de la lista antes de aplicar")
 
 def guardaContacto():
-    pass
+    if len(campoNombre.get()) <= 0 or len(campoApellidos.get()) <= 0 or len(campoTelefono.get()) <= 0 or len(campoEmail.get()) <= 0:
+        mensajes.config(text="Se deben completar todos los campos antes de guardar el contacto en la agenda")
+    else:
+        numeroContactos = len(tabla.get_children())#numero de elementos en la tabla
+        n = numeroContactos +1
+        elementoN = f'elemento{numeroContactos + 1}'
+        ContactoN = f'Contacto{numeroContactos + 1}'
+        #aniadir nuevo contacto
+        tabla.insert('',n, elementoN, text=ContactoN, values=(
+            campoNombre.get(), campoApellidos.get(), campoTelefono.get(), campoEmail.get()))
+        mensajes.config(text="Contacto añadido correctamente")
+
+def limpiaFormulario():
+    campoNombre.delete(0,tk.END)
+    campoApellidos.delete(0,tk.END)
+    campoTelefono.delete(0,tk.END)
+    campoEmail.delete(0,tk.END)
+    mensajes.config(text="")
     
 def seleccionaElemento(evento):
-    seleccionado = arbol.focus()
-    print(seleccionado)
-    valores = arbol.item(seleccionado,'values')
-    print(valores)
-    
+    seleccionado = tabla.focus() #elemento que se ha seleccionado
+    valores = tabla.item(seleccionado,'values') #valores del elemento seleccionado
+    mensajes.config(text=f'Contacto seleccionado: {valores}') #formateo de valores    
 
 #VENTANA Y ESTILOS
 ventana = tk.Tk()
@@ -50,29 +69,35 @@ campoTelefono.grid(row=3, column=2)
 campoEmail.grid(row=3, column=3)
 
 #LISTENER FORMULARIO
+campoNombre.bind("<FocusOut>", validarCampo)
+campoApellidos.bind("<FocusOut>", validarCampo)
+campoTelefono.bind("<FocusOut>", validarCampo)
+campoEmail.bind("<FocusOut>", validarCampo)
 
-
-#BOTON GUARDAR
+#BOTON GUARDAR Y LIMPIAR
 botonGuardar = ttk.Button(ventana,text="Guardar contacto",command=guardaContacto)
-botonGuardar.grid(row=4, column=0, columnspan=4, pady=10)
+botonGuardar.grid(row=4, column=1, pady=10)
 
-mensajes = ttk.Label(ventana,text="mensajes")
+botonLimpiar = ttk.Button(ventana,text="Limpiar formulario",command=limpiaFormulario)
+botonLimpiar.grid(row=4, column=2, pady=10)
+
+mensajes = ttk.Label(ventana,text="")
 mensajes.grid(row=5, column=0, columnspan=4)
 
 #TABLA
-arbol = ttk.Treeview(ventana,columns=('nombre','apellidos','telefono','email'))
-arbol.heading("#0",text="Identificador")
-arbol.heading("nombre",text="Nombre")
-arbol.heading("apellidos",text="Apellidos")
-arbol.heading("telefono",text="Teléfono")
-arbol.heading("email",text="Correo electronico")
+tabla = ttk.Treeview(ventana,columns=('nombre','apellidos','telefono','email'))
+tabla.heading("#0",text="Identificador")
+tabla.heading("nombre",text="Nombre")
+tabla.heading("apellidos",text="Apellidos")
+tabla.heading("telefono",text="Teléfono")
+tabla.heading("email",text="Correo electronico")
 
-arbol.insert('','0','elemento1',text='Cliente1', values=("Claudia","Rubio Menco","12345678","claudia@email.com"))
-arbol.insert('','1','elemento2',text='Cliente2', values=("Mateo","Rodriguez Perez", "87654321","mateo@email.com"))
+tabla.insert('','0','elemento1',text='Contacto1', values=("Claudia","Rubio Menco","12345678","claudia@email.com"))
+tabla.insert('','1','elemento2',text='Contacto2', values=("Mateo","Rodriguez Perez", "87654321","mateo@email.com"))
 
-arbol.grid(row=6, column=0, columnspan=4, padx=50, pady=10)
+tabla.grid(row=6, column=0, columnspan=4, padx=50, pady=10)
 
-arbol.bind('<<TreeviewSelect>>',seleccionaElemento)
+tabla.bind('<<TreeviewSelect>>',seleccionaElemento)
 
 #CAMBIAR EL ESTILO
 desplegable = ttk.Combobox(ventana,values=estilo.theme_names())
